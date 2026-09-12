25cc57517b4a0f68773e178b5117c2bfdf7d368c
Soumyaranjan Panda <Soumyaranjanpanda910@gmail.com>
2026-09-12 21:51:10 +0530

    purge 0.8.0

--- Formula/purge.rb
+++ Formula/purge.rb
@@ -1,8 +1,8 @@
 class Purge < Formula
   desc "Reclaim the regenerable junk on your Mac: build output, caches, Xcode, agents"
   homepage "https://purge.bixai.dev/"
-  url "https://registry.npmjs.org/purge-cli/-/purge-cli-0.7.1.tgz"
-  sha256 "ef46e9196b50571587463b0299d7cb125d4f3254e2c896a19d32064a71d08f15"
+  url "https://registry.npmjs.org/purge-cli/-/purge-cli-0.8.0.tgz"
+  sha256 "11b196fed3c25f5001d620f003804b4f8f696267f4b5d42f5b3de9862d009daf"
   license "MIT"
 
   depends_on :macos
